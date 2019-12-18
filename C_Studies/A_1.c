beb208f38098eb02cdcfa8113fd6c34bc8efa267
Ricardo S. Chao <richiechao95@gmail.com>
2019-12-18 22:54:07 +0800

    [new] git study notes

--- C_Studies/A_1.c
+++ C_Studies/A_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("This is my first c program")
     return 0;
 }
---
+++ Git/Bash_Profile/.bashrc
@@ -0,0 +1,1 @@
+PATH=$PATH=:/c/Users/ricardo/Anaconda3/:/c/Users/ricardo/Anaconda3/Scripts/:
