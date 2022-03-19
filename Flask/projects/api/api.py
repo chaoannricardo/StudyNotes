d9b9ae59ec240101b28d7317c0a202478319473b
chaoannricardo <chaoannricardo@protonmail.com>
2022-03-19 22:26:43 +0800

    [new] flask materials

---
+++ Flask/projects/api/api.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
+if __name__ == '__main__':
+    pass
